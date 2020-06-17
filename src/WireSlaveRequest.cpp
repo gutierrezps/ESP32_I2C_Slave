4b37a32155a6ec309fd850f2d1741b4c90f09b3f
Gutierrez PS <gutierrez@live.jp>
2020-06-17 02:13:26 -0300

    Added Slave request helper class

---
+++ src/WireSlaveRequest.cpp
@@ -0,0 +1,124 @@
+#include "WireSlaveRequest.h"
+
+WireSlaveRequest::WireSlaveRequest(TwoWire &wire, uint8_t address, uint16_t readLength)
+    :wire_(wire)
+    ,address_(address)
+    ,readLength_(readLength)
+    ,retryDelay_(10)
+    ,maxAttempts_(5)
+    ,lastStatus_(NONE)
+{
+}
+
+bool WireSlaveRequest::request(uint8_t address = 0)
+{
+    if (address != 0) {
+        address_ = address;
+    }
+
+    uint8_t attempts = 0;
+
+    WireUnpacker unpacker;
+
+    bool sendTrigger = true;
+
+    while (attempts < maxAttempts_) {
+        if (sendTrigger) {
+            triggerUpdate();
+            sendTrigger = false;
+        }
+
+        // wait until slave fills its output buffer
+        delay(retryDelay_ * (attempts + 1));
+
+        uint8_t returned = wire_.requestFrom(address_, readLength_);
+        if (returned == 0) {
+            lastStatus_ = SLAVE_NOT_FOUND;
+            return false;
+        }
+
+        while (wire_.available()) {
+            uint8_t c = wire_.read();
+            unpacker.write(c);
+        }
+
+        if (unpacker.available()) {
+            // a complete packet was read
+            break;
+        }
+        else if (unpacker.hasError()) {
+            // retry request
+            unpacker.reset();
+            sendTrigger = true;
+        }
+
+        ++attempts;
+    }
+
+    if (attempts == maxAttempts_) {
+        if (unpacker.hasError()) {
+            lastStatus_ = PACKET_ERROR;
+        }
+        else {
+            // not sure what could lead here
+            lastStatus_ = MAX_ATTEMPTS;
+        }
+        return false;
+    }
+
+    // copy payload bytes to rxBuffer
+    rxIndex_ = 0;
+    while (unpacker.available()) {
+        rxBuffer_[rxIndex_] = unpacker.read();
+        ++rxIndex_;
+    }
+
+    rxLength_ = rxIndex_;
+    rxIndex_ = 0;
+    lastStatus_ = PACKET_READ;
+
+    return true;
+}
+
+String WireSlaveRequest::lastStatusToString() const
+{
+    switch (lastStatus_) {
+    case NONE: return "none";
+    case PACKET_READ: return "packet read";
+    case SLAVE_NOT_FOUND: return "slave not found";
+    case PACKET_ERROR: return "packet error";
+    case MAX_ATTEMPTS: return "max attempts";
+    default: return "unknown";
+    }
+}
+
+size_t WireSlaveRequest::available()
+{
+    if (lastStatus_ != PACKET_READ) {
+        return 0;
+    }
+
+    return rxLength_ - rxIndex_;
+}
+
+int WireSlaveRequest::read(void)
+{
+    int value = -1;
+    if (lastStatus_ == PACKET_READ && rxIndex_ < rxLength_) {
+        value = rxBuffer_[rxIndex_];
+        ++rxIndex_;
+    }
+    return value;
+}
+
+void WireSlaveRequest::triggerUpdate()
+{
+    WirePacker packer;
+    packer.end();
+
+    wire_.beginTransmission(address_);
+    while (packer.available()) {
+        wire_.write(packer.read());
+    }
+    wire_.endTransmission();
+}
---
+++ src/WireSlaveRequest.h
@@ -0,0 +1,123 @@
+/**
+ * @file WireSlaveRequest.h
+ * @author Gutierrez PS <https://github.com/gutierrezps>
+ * @brief Helper class to read packed data from an ESP32 slave
+ * @date 2020-06-17
+ * 
+ * This class is made to wrap the requestFrom() from default
+ * Wire library. It requests data from an ESP32 slave (packed
+ * using WirePacker) until a complete packet is received. It
+ * also retries and handles errors.
+ * 
+ * After creating an object, call the request() method.
+ * If a packet was read correctly, use methods available()
+ * and read() to get the payload bytes.
+ * 
+ * Use setRetryDelay() and setAttemps() if errors are
+ * happening frequently.
+ * 
+ */
+#ifndef WireSlaveRequest_h
+#define WireSlaveRequest_h
+
+#include <stdint.h>
+#include <Wire.h>
+#include "WirePacker.h"
+#include "WireUnpacker.h"
+
+class WireSlaveRequest
+{
+public:
+    enum Status
+    {
+        NONE,
+        PACKET_READ,
+        SLAVE_NOT_FOUND,
+        PACKET_ERROR,
+        MAX_ATTEMPTS,
+    };
+
+    /**
+     * Construct a new WireSlaveRequest object
+     * 
+     * @param wire          TwoWire object (Wire or Wire1)
+     * @param address       slave address
+     * @param readLength    max packet length
+     */
+    WireSlaveRequest(TwoWire &wire, uint8_t address, uint16_t readLength);
+
+    /**
+     * Delay in milliseconds between retry attempts
+     */
+    void setRetryDelay(unsigned long retryDelay)
+    {
+        retryDelay_ = retryDelay;
+    }
+
+    /**
+     * Number of read attempts before giving up with an error status
+     * 
+     * @param attempts 
+     */
+    void setAttempts(uint8_t attempts)
+    {
+        maxAttempts_ = attempts;
+    }
+
+    /**
+     * @brief Requests data from an ESP32 I2C slave, packed with WirePacker.
+     * 
+     * If a packet was successfully retrieved, methods available() and
+     * read() can be used to retrieve the payload. Otherwise, check
+     * lastStatus() or lastStatusString() to see what went wrong.
+     * 
+     * @param address   slave address (optional)
+     * @return true     a new packet was read
+     * @return false    something wrong happened
+     */
+    bool request(uint8_t address = 0);
+
+    Status lastStatus() const
+    {
+        return lastStatus_;
+    }
+
+    String lastStatusToString() const;
+
+    /**
+     * Returns how many payload bytes are available to be read, after
+     * a packet was successfully read through request().
+     * 
+     * @return size_t
+     */
+    size_t available();
+
+    /**
+     * Read the next available payload byte. At each call,
+     * the value returned by available() will be decremented.
+     * 
+     * @return int  -1 if there are no bytes to be read
+     */
+    int read();
+
+private:
+    TwoWire &wire_;
+    uint8_t address_;
+    uint8_t readLength_;
+    unsigned long retryDelay_;
+    uint8_t maxAttempts_;
+    Status lastStatus_;
+
+    uint8_t rxBuffer_[UNPACKER_BUFFER_LENGTH];
+    uint16_t rxLength_;
+    uint16_t rxIndex_;
+
+    /**
+     * @brief Sends an empty packet to the slave in order to trigger
+     * its output buffer update.
+     * 
+     */
+    void triggerUpdate();
+};
+
+#endif
